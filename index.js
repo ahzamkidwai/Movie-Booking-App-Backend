145c9744d3704d5880f5ca0ee8a2f151dacfac25
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-04-14 11:17:38 +0530

    Add Movie Schema Model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const connectMongoDB = require('./config/db.js');
+const Movie = require('./models/movie.model.js');
 
 dotenv.config();
 
@@ -11,18 +12,28 @@ const PORT = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     return res.send("Welcome to Home Page")
 })
 
-app.get("/home", (req, res)=>{
+app.get("/home", (req, res) => {
     return res.json({
-        success:true,
-        message:"Welcome to Home Page"
+        success: true,
+        message: "Welcome to Home Page"
     })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log("Listening to port : ", PORT)
     connectMongoDB();
+    await Movie.create({
+        "name": "Dangal",
+        "description": "A former wrestler trains his daughters to become world-class wrestlers.",
+        "casts": ["Aamir Khan", "Fatima Sana Shaikh", "Sanya Malhotra"],
+        "trailerUrl": "https://www.youtube.com/watch?v=x_7YlGv9u1g",
+        "language": "Hindi",
+        "releaseDate": "2016-12-23",
+        "director": "Nitesh Tiwari",
+        "releaseStatus": "RELEASED"
+    })
 })
