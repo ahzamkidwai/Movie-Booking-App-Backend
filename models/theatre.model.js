bc7080dd7c9935baa7a56e6aa0d6b3335eb7fb61
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-05-13 00:47:49 +0530

    Feat: Add Theatre Model

---
+++ models/theatre.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+/**
+* Define the schema of the theatre resource to be stored in the DB.
+*/
+
+const theatreSchema = new mongoose.Schema({
+    name: {
+        type: String, required: true
+    },
+    description: {
+        type: String
+    },
+    city: {
+        type: String, required: true
+    },
+    pinCode: {
+        type: Number, required: true, minLength: 6, maxLength: 6
+    }, 
+    address: {
+        type: String
+    }
+}, { timestamps: true });
+
+const Theatre = mongoose.model("Theatre", theatreSchema);
+module.exports = Theatre;
